8c7c9dea7009c5ae2e017d124e8fb3c591be8677
Ali-TM-original <76595455+Ali-TM-original@users.noreply.github.com>
2021-05-02 18:02:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 class main{
     constructor(previous, current){
-        this.previous = previous;
-        this.current = current;
+        this.previous_data = previous;
+        this.current_data = current;
         this.clear()
     }
 
@@ -11,21 +11,64 @@ class main{
         this.operation = undefined;
     }
 
-    delete(){}
-
-    appendnumber(number){
-        console.log(number)
-        if (number === '.' && this.current.includes('.')) return;
-        this.current = this.current.toString() + number.toString();
-        console.log(this.current)
+    delete(){
+        this.current = this.current.toString().slice(0, -1)
     }
 
-    chooseoperation(operation){}
+    appendnumber(number){
 
-    main_do(){}
+        if (number === '.' && this.current.includes('.')) return;
+        this.current = this.current.toString() + number.toString();
+    }
 
+    chooseoperation(operation){
+        if(this.current === '') return;
+        /* calling main method if operation is present */
+        if(this.operation !== ''){
+
+            this.main_do();
+        }
+        /*   Previous text becomes current text and current text becomes empty str   */
+        this.operation = operation;
+        this.previous = this.current;
+        this.current = ''
+    }
+        
+    main_do() {
+        let computation
+        const prev = parseFloat(this.previous)
+        const current = parseFloat(this.current)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+          case '+':
+            computation = prev + current
+            break
+          case '-':
+            computation = prev - current
+            break
+          case '*':
+            computation = prev * current
+            break
+          case '÷':
+            computation = prev / current
+            break
+          default:
+            return
+        }
+        this.current = computation
+        this.operation = undefined
+        this.previous = ''
+      }
     updatedisplay(){
-        this.current.innerText = this.current;
+        /* Updating the Current text */
+        this.current_data.innerText = this.current;
+
+        if (this.operation != null){
+            this.previous_data.innerText = `${this.previous} ${this.operation}`
+        }
+        /* Updating the Previous text */
+        
+
     }
 }
 
@@ -41,12 +84,39 @@ const current_data = document.querySelector('[cur_data]');
 
 
 
+
+/*  Initializing a object of the class (python term don't know about js) */
 const calculator = new main(prev_data, current_data);
 
-data_btn.forEach(button =>{
-    button.addEventListener("click", ()=> {
-        calculator.appendnumber(button.innerText)
-        calculator.updatedisplay()
-    })
 
-})
+/*  EVENT LISTENER SECTION */
+
+data_btn.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendnumber(button.innerText)
+      calculator.updatedisplay()
+    })
+  })
+
+operation_btn.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseoperation(button.innerText)
+      calculator.updatedisplay()
+    })
+  })
+
+
+  equal_btn.addEventListener('click', button => {
+    calculator.main_do()
+    calculator.updatedisplay()
+  })
+  
+  ac_btn.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updatedisplay()
+  })
+  
+  delete_btn.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updatedisplay()
+  })
